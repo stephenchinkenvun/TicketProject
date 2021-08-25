41c4fcf8ae4280406a538f9c74a3fafb3bb88b1f
senyee18 <86588857+senyee18@users.noreply.github.com>
2021-08-25 17:44:14 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler'
 
+import OnBoardingScreen from './screens/OnBoardingScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 import TabNavigator from './navigation/tabs';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return(
     <NavigationContainer>
-      <TabNavigator />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Onboarding" component={OnBoardingScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
+}
+
+const Root = () => {
+  return(
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
+  )
 }
 
 export default App;
