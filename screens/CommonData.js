f43c1e5efe271799bdc5a5da4430a7d85b3d0ef3
jalencheng <64760269+jalencheng@users.noreply.github.com>
2021-09-03 00:23:19 +0800

    Add files via upload

---
+++ screens/CommonData.js
@@ -0,0 +1,59 @@
+exports.getValue = (array, key) => {
+  return array.filter(o => o.key === key)[0].value;
+};
+
+exports.departures = [
+  { key: '01', value: 'Johor' },
+  { key: '02', value: 'Kedah' },
+  { key: '03', value: 'Kelantan' },
+  { key: '14', value: 'Kuala Lumpur' },
+  { key: '15', value: 'Labuan' },
+  { key: '04', value: 'Melaka' },
+  { key: '05', value: 'Negeri Sembilan' },
+  { key: '06', value: 'Pahang' },
+  { key: '07', value: 'Penang' },
+  { key: '08', value: 'Perak' },
+  { key: '09', value: 'Perlis' },
+  { key: '16', value: 'Putrajaya' },
+  { key: '12', value: 'Sabah' },
+  { key: '13', value: 'Sarawak' },
+  { key: '10', value: 'Selangor' },
+  { key: '11', value: 'Terengganu' },
+];
+
+exports.arrivals = [
+  { key: '01', value: 'Johor' },
+  { key: '02', value: 'Kedah' },
+  { key: '03', value: 'Kelantan' },
+  { key: '14', value: 'Kuala Lumpur' },
+  { key: '15', value: 'Labuan' },
+  { key: '04', value: 'Melaka' },
+  { key: '05', value: 'Negeri Sembilan' },
+  { key: '06', value: 'Pahang' },
+  { key: '07', value: 'Penang' },
+  { key: '08', value: 'Perak' },
+  { key: '09', value: 'Perlis' },
+  { key: '16', value: 'Putrajaya' },
+  { key: '12', value: 'Sabah' },
+  { key: '13', value: 'Sarawak' },
+  { key: '10', value: 'Selangor' },
+  { key: '11', value: 'Terengganu' },
+];
+
+
+exports.departureTime = [
+  { key: '01', value: '08:30AM' },
+  { key: '02', value: '09:30AM' },
+  { key: '03', value: '10:30AM' },
+  { key: '04', value: '11:30AM' },
+  { key: '05', value: '01:30PM' },
+  { key: '06', value: '02:30PM' },
+  { key: '07', value: '03:30PM' },
+  { key: '08', value: '04:30PM' },
+];
+
+exports.price = [
+  { key: '01', value: '10  Normal' },
+  { key: '02', value: '15  Deluxe' },
+];
+
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,233 @@
-import { NavigationContainer } from '@react-navigation/native';
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, StyleSheet, ScrollView, View, Text, Button, DatePickerAndroid,TouchableHighlight } from 'react-native';
+import { InputWithLabel, PickerWithLabel, AppButton } from '../UI';
+
+let config = require('../Config');
+let common = require('./CommonData');
+
+type Props = {};
+export default class HomeScreen extends Component<Props> {
+  static navigationOptions = {
+    title: 'Home',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      price: '',
+      departure: '',
+      arrival: '',
+      departureTime: '',
+      date: 0,
+    };
+
+    this.state = {
+      selectedStartDate: null,
+    };
+    this.onDateChange = this.onDateChange.bind(this);
+
+    this._store = this._store.bind(this);
+  }
+
+  onDateChange(date) {
+    this.setState({
+      selectedStartDate: date,
+    });
+  }
+
+  openDatePicker = async () => {
+    try {
+      const {action, year, month, day} = await DatePickerAndroid.open({
+        date: this.state.date,
+        minDate: new Date(2000, 0, 1),
+        maxDate: new Date(2099, 11, 31),
+        mode: 'calendar',
+      });
+      if (action !== DatePickerAndroid.dismissedAction) {
+        let selectedDate = new Date(year, month, day);
+        let selectedDatesec = selectedDate.getTime();
+        this.setState({
+          date : selectedDatesec,
+        });
+      }
+    } catch ({code, message}) {
+      console.warn('Cannot open date picker', message);
+    }
+  }
 
 
-const HomeScreen = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
+  _store() {
+    let url = config.settings.serverPath + '/api/bus';
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        price: this.state.price,
+        departure: this.state.departure,
+        arrival: this.state.arrival,
+        departureTime: this.state.departureTime,
+        arrivalTime: this.state.arrivalTime,
+        day: this.state.day,
+        date: this.state.date,
+      }),
+    })
+      .then(response => {
+        if (!response.ok) {
+          Alert.alert('Error', response.status.toString());
+          throw Error('Error ' + response.status);
+        }
+
+        return response.json();
+      })
+      .then(responseJson => {
+        if (responseJson.affected > 0) {
+          Alert.alert(
+            'Record Saved',
+            'Record has been saved',
+            [
+              {
+                onPress:() => navigation.navigate('Ticket'),
+              },
+            ],
+          );
+        } else {
+          Alert.alert('Error saving record');
+        }
+
+
+        //this.props.navigation.getParam('refresh')();
+        //this.props.navigation.goback();
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  render() {
+
+      let date=this.state.date;
+      var dateday = new Date(date).getDate();
+      var month = new Date(date).getMonth() ;
+      var year = new Date(date).getFullYear();
+      var day = new Date(date).getDay();
+      var daysText = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+      var monthsText = [
+        'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep', 'Oct','Nov','Dec',];
+      var dayName = daysText[day];
+      var monthName = monthsText[month];
+      var newdate = dayName + ', ' + dateday + ' ' + monthName + ', ' + year;
+  
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.text}> Bus Ticket Booking</Text>
+
+        <PickerWithLabel
+          style={styles.picker}
+          label={'From'}
+          items={common.departures}
+          mode={'dialog'}
+          value={this.state.departure}
+          onValueChange={(itemValue, itemIndex) => {
+            this.setState({ departure: itemValue });
+          }}
+          
+          orientation={'vertical'}
+          textStyle={{ fontSize: 24 }}
+        />
+        <PickerWithLabel
+          style={styles.picker}
+          label={'To'}
+          items={common.arrivals}
+          mode={'dialog'}
+          value={this.state.arrival}
+          onValueChange={(itemValue, itemIndex) => {
+            this.setState({ arrival: itemValue });
+          }}
+          orientation={'vertical'}
+          textStyle={{ fontSize: 24 }}
+        />
+        <PickerWithLabel
+          style={styles.picker}
+          label={'Choose a time'}
+          items={common.departureTime}
+          mode={'dialog'}
+          value={this.state.departureTime}
+          onValueChange={(itemValue, itemIndex) => {
+            this.setState({ departureTime: itemValue });
+          }}
+          orientation={'vertical'}
+          textStyle={{ fontSize: 24 }}
+        />
+
+        <TouchableHighlight
+        underlayColor={'#cccccc'}
+          onPress={ this.openDatePicker }>
+          <View style={styles.container}>
+            <InputWithLabel
+              style={styles.input}
+              label={'Choose a Date'}
+              value={newdate}
+              editable={false}
+              onChangeText={(date) => {
+                this.setState({date: itemvalue});
+            }}
+            />
+          </View></TouchableHighlight>
+          <PickerWithLabel
+          style={styles.picker}
+          label={'Choose a Class'}
+          items={common.price}
+          mode={'dialog'}
+          value={this.state.price}
+          onValueChange={(itemValue, itemIndex) => {
+            this.setState({ price: itemValue });
+          }}
+          orientation={'vertical'}
+          textStyle={{ fontSize: 24 }}
+        />
+        <AppButton
+          style={styles.button}
+          title={'Book'}
+          theme={'primary'}
+          onPress={this._store}
+        />
+      </ScrollView>
+    );
+  }
 }
 
-//must export to run bottom tab
-export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#fff',
+  },
+  input: {
+    fontSize: 16,
+    color: '#000099',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+
+  picker: {
+    color: '#000099',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 10,
+    marginBottom: 150,
+  },
+});
